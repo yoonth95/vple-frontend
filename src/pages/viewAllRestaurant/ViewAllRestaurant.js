758616cba4a18b29092db80d44b70ac74e032280
7548ck@naver.com <7548ck@naver.com>
2023-02-12 02:46:12 +0900

    feat: restaurant 전체보기

--- src/pages/viewAllRestaurant/ViewAllRestaurant.js
+++ src/pages/viewAllRestaurant/ViewAllRestaurant.js
@@ -1,38 +1,75 @@
 import DetailHeader from '../../components/titleHeader/TitleHeader'
 import DetailSearchBar from '../../components/searchBar/DetailSearchBar'
 import CardButton from '../../components/guideCardButton/GuideCardButton';
-import {UlStyle} from '../../styles/UlStyle';
 
-export default function ViewAllRestaurant() {
+import {
+    SearchContainer,
+    WrapSelection,
+    CardContainer,
+    WrapCards,
+    WrapCard,
+
+} from './ViewAllRestaurantStyle';
+
+function ViewAllRestaurant() {
 
     const cards = [
-        { name: '연화바루' },
-        { name: '향적원' },
-        { name: '연화바루' },
-        { name: '향적원' },
-        { name: '연화바루' },
-        { name: '향적원' },
-        { name: '연화바루' },
-        { name: '향적원' },
-        { name: '연화바루' },
-        { name: '향적원' },
-        { name: '연화바루' },
-        { name: '향적원' },
+        '베지테리언 타코',
+        '베지테리언 타코',
+        '베지테리언 타코',
+        '베지테리언 타코',
+        '베지테리언 타코',
+        '베지테리언 타코',
+        '베지테리언 타코',
+        '베지테리언 타코',
+        '베지테리언 타코',
+        '베지테리언 타코',
+        '베지테리언 타코',
+        '베지테리언 타코',
+        '베지테리언 타코',
+        '베지테리언 타코',
     ];
 
+    const provinceList = ['서울', '경기도', '인천', '충청북도', '충청남도', '전라북도', '전라남도,', '강원도', '경상북도', '경상남도'];
+    const locationList = ['김포시', '의정부시', '인천광역시', '부산', '목포시', '강릉시', '대구광역시'];
 
-    return(
+    return (
         <>
-            <DetailHeader title="추천 식당 전체 보기"/>
-            <DetailSearchBar/>
-            <UlStyle>
-                {cards.map(card => (
-                    <CardButton
-                        title={card.name}
-                    />
+            <DetailHeader title="추천 식당 전체 보기" />
 
-                ))}
-            </UlStyle>
+            <SearchContainer>
+                <WrapSelection>
+                    <select className='select-box'>
+                        {provinceList.map((province, index) => {
+                            return <option className='option'>{province}</option>
+                        })}
+                    </select>
+                    <select className='select-box'>
+                        {locationList.map((city, index) => {
+                            return <option className='option'>{city}</option>
+                        })}
+                    </select>
+                </WrapSelection>
+            </SearchContainer>
+
+            <CardContainer>
+                <WrapCards>
+                    {
+                        cards.map((value, index) => {
+                            return (
+                                <WrapCard>
+                                    <img className='img-photo' />
+                                    <div className='div-content'>
+                                        <span className='plan-title'>{value}</span>
+                                    </div>
+                                </WrapCard>
+                            )
+                        })
+                    }
+                </WrapCards>
+            </CardContainer>
         </>
     );
 }
+
+export default ViewAllRestaurant
