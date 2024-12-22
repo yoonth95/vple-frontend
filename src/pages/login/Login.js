f2f7077d79f8d9019a41d6229a6b732b97ec1934
윤태현 <78673090+yoonth95@users.noreply.github.com>
2024-12-22 21:01:43 +0900

    Refactor: kakao 로그인 리다이렉트 URI 수정

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -5,7 +5,8 @@ import kakao from "../../asset/kakao.png";
 import { Container, WrapLogin } from "./LoginStyle";
 
 const CLIENT_ID = "9bace5ab316d4073d7412f542dd4f2c8";
-const REDIRECT_URI = `https://${window.location.host}/user/kakao/callback`;
+// const REDIRECT_URI = `https://${window.location.host}/user/kakao/callback`;
+const REDIRECT_URI = `https://${window.location.host}/auth`;
 const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
 export default function Login() {
