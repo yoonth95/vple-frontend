b1605a5f154d26cfffb6352a4702213ca5700f16
윤태현 <78673090+yoonth95@users.noreply.github.com>
2024-12-24 09:47:03 +0900

    Refactor: catch 에러 추가가

--- src/pages/login/KakaoAuthHandle.jsx
+++ src/pages/login/KakaoAuthHandle.jsx
@@ -1,34 +1,34 @@
-import axios from 'axios'
-import { useEffect } from 'react'
+import axios from "axios";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import {Url} from '../Url';
+import { Url } from "../Url";
 
 const KakaoAuthHandle = (props) => {
+  useEffect(() => {
+    let code = new URL(window.location.href).searchParams.get("code");
 
-    useEffect(() => {
-      
-      let code = new URL(window.location.href).searchParams.get('code');
+    const kakaoLogin = async () => {
+      axios
+        .get(`${Url}/api/oauth/token?code=${code}`)
+        .then((res) => {
+          console.log(res);
 
-      const kakaoLogin = async () => {
-        axios
-          .get(`${Url}/api/oauth/token?code=${code}`)
-          .then((res) => {
-            console.log(res);
+          localStorage.setItem("token", res.headers.get("Authorization"));
+          localStorage.setItem("token_body", res.body);
 
-            localStorage.setItem('token', res.headers.get("Authorization"));
-            localStorage.setItem('token_body', res.body);
-            
+          window.location.href = "/";
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+    kakaoLogin();
+  }, [props.history]);
 
-            window.location.href = "/";
-          })
-      }
-      kakaoLogin()
-    }, [props.history])
-  
-    return (
-      <>
-        <div>로그인 중입니다. 잠시만 기다려주세요.</div>
-      </>
-    )
-  }
-  export default KakaoAuthHandle
+  return (
+    <>
+      <div>로그인 중입니다. 잠시만 기다려주세요.</div>
+    </>
+  );
+};
+export default KakaoAuthHandle;
